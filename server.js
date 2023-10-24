dbb1b3111e388f9150c6f61dead917010dbf3231
Kevin <xkerts@outlook.com>
2023-10-23 23:21:34 -0500

    Push inicial

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.write("<h1>Hello World<h1/>");
+});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
